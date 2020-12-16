dab4163bc75b818f7687599827fad578c6327f26
feifei14119 <feifei_14119@163.com>
2020-12-16 13:21:56 +0800

    tiny fix for dpp

--- isaDpp/isaDpp_v3.s
+++ isaDpp/isaDpp_v3.s
@@ -117,6 +117,7 @@ START_PROG:
      * row2: | 32, 33, 34, 35,| 36, 37, 38, 39,| 40, 41, 42, 43,| 44, 45, 46, 47 |
      * row3: | 48, 49, 50, 51,| 52, 53, 54, 55,| 56, 57, 58, 59,| 60, 61, 62, 63 |
      *
+     * v_mov_b32_dpp		v[v_c],					v[v_a]          quad_perm:[3,2,1,0]
      * v_mov_b32_dpp		v[v_c],					v[v_a]          row_mirror
      * v_mov_b32_dpp		v[v_c],					v[v_a]          row_share:1 // not work
      * v_mov_b32_dpp		v[v_c],					v[v_a]          row_xmask:1 // not work
